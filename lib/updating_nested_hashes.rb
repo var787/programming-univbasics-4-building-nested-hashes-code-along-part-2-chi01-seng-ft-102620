f4535a3587155c26c9ad3de418f12ec9a0571791
Rakshan Hegde <hegde.rakshan@gmail.com>
2020-10-10 03:38:29 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -22,6 +22,7 @@ def update_status
 
   epic_tragedy[:montague][:hero][:status]="dead"
   epic_tragedy[:capulet][:heroine][:status]="dead"
+  epic_tragedy[:additional_characters]=[{name:"Prince Escalus"},{name:"Apothecary"}]
 
 
 
