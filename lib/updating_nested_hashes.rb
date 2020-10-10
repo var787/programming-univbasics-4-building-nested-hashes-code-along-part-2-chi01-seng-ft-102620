98f7b4d75d950d03c193a93cfc7b0dfe12949580
Rakshan Hegde <hegde.rakshan@gmail.com>
2020-10-10 03:45:42 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -28,7 +28,7 @@ def update_status
 
 
 
-  # After your code, the following code will return the altered hash. This method must only return the modified epic_tragedy hash
+end
   epic_tragedy
 end
 
@@ -56,6 +56,7 @@ def add_characters
 
    epic_tragedy[:additional_characters]<<[:name "Prince Escalus"]
    epic_tragedy[:additional_characters]<<[:name "Apothecary"]
+ end
 
 
 
